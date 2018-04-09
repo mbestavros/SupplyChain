7cc3191d4e2ac8c498775c64016e082b455370ce
shreya726 <shreya@bu.edu>
2018-04-09 19:15:22 -0400

    moved transactions to bm

--- blockmanager/blockmanager.go
+++ blockmanager/blockmanager.go
@@ -21,6 +21,29 @@ type Block struct {
 type Blockmanager struct {
 }
 
+type Action string
+
+
+//Enum for all types of transactions
+const (
+	Create Action = "Create"
+	Exchange Action = "Exchange"
+	Consume Action = "Consume"
+	Make Action = "Make"
+	Split Action = "Split"
+)
+
+//Struct to represent all Transactions
+type Transaction struct{
+	Type Action
+	OriginUser int64
+	DestinationUser int64
+	InitialTimestamp int
+	FinalTimestamp int
+	//Hash probably goes here as well @Sean
+
+}
+
 var bm Blockmanager
 
 // make sure block is valid by checking index, and comparing the hash of the previous block
