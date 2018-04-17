e7f8adef771195a01d3f038f8990f5648f662916
shreya726 <shreya@bu.edu>
2018-04-17 14:35:28 -0400

    small errors in bm

--- src/server/server.go
+++ src/server/server.go
@@ -1,13 +1,58 @@
-package main
+package server
 
-//TODO: handle transactions
+import 
+(
+	"grouper"
+	"blockmanager"
+)
 
-//TODO: start network in case of genesis block
+type Server struct {
+	//http probably
+}
+
+var sr Server 
+
+//start network in case of genesis block
+func (sr *Server) genesis(){
+	gr := grouper.Grouper{}
+	bm := blockmanager.Blockmanager{}
+	genesisBlock := bm.Genesis()
+	gr.StartNetwork("test","test","Test")
+}
 
 //TODO: join network if not genesis
+func (sr *Server) join(){
+	gr := grouper.Grouper{}
+	bm := blockmanager.Blockmanager{}
+	
+	gr.JoinNetwork("test", "Test", "test", "test", "Test")
+	//broadcast user info to all users 
+	//http 
+
+	//get existing blockchain 
+}
 
 //TODO: send blocks 
 // go routine for proof of work new block
+func (sr *Server) sendBlock(block Block) {
+	gr := grouper.Grouper{}
+	bm := blockmanager.Blockmanager{}
+	go func() {
+		newBlock := bm.GenerateBlock(block, "transaction info") //pass old block, new info
+	}()
+
+}
 
 //TODO: receive blocks 
+func (sr *Server) start(){
+	gr := grouper.Grouper{}
+	bm := blockmanager.Blockmanager{}
+	for {
+		newBlock := false
+		//newBlock := <- bcServer.something
 
+		if newBlock {
+			//something 
+		}
+	}
+}
