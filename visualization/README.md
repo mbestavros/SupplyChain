ffdf34d1e8bf706472a0e7c480e6de8c8f5ee001
mbestavros <markbest@bu.edu>
2018-04-24 14:32:01 -0400

    Add README and a few fixes

---
+++ visualization/README.md
@@ -0,0 +1,22 @@
+This is the public-facing server that we will use to visualize our blockchain in-class.
+
+## Setup
+
+- Ensure `Python3` is installed (tested on 3.6)
+- Ensure `Flask is` installed (`pip install Flask`)
+
+## Running the server
+
+`python3 server.py`
+
+Navigate to the URL/IP provided.
+
+## Troubleshooting
+
+To fix the error Shreya ran into while running the server:
+
+Run: 
+
+`/Applications/Python\ 3.6/Install\ Certificates.command`
+
+in a command line.
--- visualization/server.py
+++ visualization/server.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/')
 def display_json():
 	# URL of the blockchain node that returns JSON. Provided URL returns an example JSON for testing.
-	blockchain_node_api_url = 'https://raw.githubusercontent.com/SMAPPNYU/ProgrammerGroup/master/LargeDataSets/sample-tweet.raw.json'
+	blockchain_node_api_url = "https://raw.githubusercontent.com/SMAPPNYU/ProgrammerGroup/master/LargeDataSets/sample-tweet.raw.json"
 	# Python JSON object to manipulate
 	blockchain_json = json.load(urllib.request.urlopen(blockchain_node_api_url))
 	# jsonify returns formatted JSON
@@ -20,5 +20,5 @@ if __name__ == '__main__':
 	app.run()
 
 	# Running on 0.0.0.0 makes it visible using my machine's IP address over local network (i.e. BU network)
-	# Test this with another machine close by
+	# TODO: Test this with another machine close by
     # app.run(host='0.0.0.0')
