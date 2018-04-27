ab05aa75e6c60a8be91dd8e2729f45abfcc08b70
mbestavros <markbest@bu.edu>
2018-04-27 18:09:39 -0400

    Add nicer frontend for visualization; finalize logic for retrieving JSON

--- visualization/server.py
+++ visualization/server.py
@@ -1,19 +1,37 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 import json
 import urllib.request
+import os
 
 # This is a simple webserver written with Flask, designed to host a visualization of the data.
 app = Flask(__name__)
 
 # Default URL will display JSON raw.
 @app.route('/')
-def display_json():
+def raw_json():
+	# jsonify returns formatted JSON
+	return jsonify(get_blockchain_json())
+
+@app.route('/index')
+def index():
+	blockchain = get_blockchain_json()
+	user = {'username': 'Mark'}
+	return render_template('index.html', user=user, blockchain=blockchain)
+
+# Helper function to grab JSON.
+def get_blockchain_json():
 	# URL of the blockchain node that returns JSON. Provided URL returns an example JSON for testing.
-	blockchain_node_api_url = "https://raw.githubusercontent.com/SMAPPNYU/ProgrammerGroup/master/LargeDataSets/sample-tweet.raw.json"
+	#blockchain_node_api_url = "file://" + os.getcwd() + "/sample.json"
+
+
+	# Assuming you start your Go server on 8080. If different, change '8081' to the port you inputted plus 1.
+	blockchain_node_api_url = "http://127.0.0.1:8081/joinGetBlock" 
+
+
 	# Python JSON object to manipulate
 	blockchain_json = json.load(urllib.request.urlopen(blockchain_node_api_url))
-	# jsonify returns formatted JSON
-	return jsonify(blockchain_json["created_at"])
+
+	return blockchain_json
 
 if __name__ == '__main__':
 	# Default value for run() starts the server on localhost, good for debugging
