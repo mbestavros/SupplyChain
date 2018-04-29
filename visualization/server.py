fea3b9d60f3302f2a200761a7c5bf7bb3ab05a2b
mbestavros <markbest@bu.edu>
2018-04-29 16:04:54 -0400

    Add full parsing logic to visualization server

--- visualization/server.py
+++ visualization/server.py
@@ -10,28 +10,40 @@ app = Flask(__name__)
 @app.route('/')
 def raw_json():
 	# jsonify returns formatted JSON
-	return jsonify(get_blockchain_json())
+	return jsonify(get_blockchain_json)
 
 @app.route('/index')
 def index():
 	blockchain = get_blockchain_json()
-	user = {'username': 'Mark'}
-	return render_template('index.html', user=user, blockchain=blockchain)
+
+	return render_template('index.html', blockchain=blockchain)
 
 # Helper function to grab JSON.
 def get_blockchain_json():
 	# URL of the blockchain node that returns JSON. Provided URL returns an example JSON for testing.
-	#blockchain_node_api_url = "file://" + os.getcwd() + "/sample.json"
+	blockchain_node_api_url = "file://" + os.getcwd() + "/sample.json"
 
-
-	# Assuming you start your Go server on 8080. If different, change '8081' to the port you inputted plus 1.
-	blockchain_node_api_url = "http://127.0.0.1:8081/joinGetBlock" 
-
+	# Production URL, assuming you start your Go server on 8080. If different, change '8081' to the port you inputted plus 1.
+	#blockchain_node_api_url = "http://127.0.0.1:8081/joinGetBlock" 
 
 	# Python JSON object to manipulate
-	blockchain_json = json.load(urllib.request.urlopen(blockchain_node_api_url))
+	blockchain = json.load(urllib.request.urlopen(blockchain_node_api_url))
 
-	return blockchain_json
+	# Parse block data to make it easier to put in HTML
+	for block in blockchain:
+		type = block['BlockTransaction']['TransactionType']
+		transactionInfo = {}
+		
+		# If transaction type is empty, we assume it's a genesis block and continue
+		if type == '':
+			transactionInfo['type'] = "Genesis"
+			transactionInfo['data'] = block['BlockTransaction']['Cr']
+		else:
+			transactionInfo['type'] = type
+			transactionInfo["data"] = block['BlockTransaction'][type[:2]]
+		block["ParsedBlockTransaction"] = transactionInfo
+
+	return blockchain
 
 if __name__ == '__main__':
 	# Default value for run() starts the server on localhost, good for debugging
